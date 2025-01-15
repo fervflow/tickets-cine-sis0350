438dfc911103c7a629079718af8e78401075d3c9
Fer <fervel1704@gmail.com>
2025-01-15 16:10:15 -0400

    Grouped batabase files

--- database/scheme.sql
+++ database/scheme.sql
@@ -119,25 +119,3 @@ CREATE TABLE Tickets
     FOREIGN KEY (asiento_id) REFERENCES Asientos (asiento_id)
 );
 GO
-
--- Carga de datos iniciales
-INSERT INTO Persona
-VALUES
-    ('1234 PO', 'Erick Fernando Velasco Flores'),
-    ('2345 OR', 'Ani Melon'),
-    ('333 CBB', 'Mar Apollo Guerrero'),
-    ('444 AR', 'Nicki Nicole'),
-    ('555 CO', 'Test Foo')
-GO
-INSERT INTO Usuario
-VALUES
-    (1, 'erick', ENCRYPTBYPASSPHRASE('upds', '123'), 'ADMINISTRADOR', 1),
-    (2, 'anime', ENCRYPTBYPASSPHRASE('upds', 'anime'), 'VENDEDOR', 1),
-    (5, 'test', ENCRYPTBYPASSPHRASE('upds', 'test'), 'VENDEDOR', 1)
-GO
-INSERT INTO Cliente
-VALUES
-    (3),
-    (4)
-GO
-
